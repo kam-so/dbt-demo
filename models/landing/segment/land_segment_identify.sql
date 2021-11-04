c137c1326b857e8ef51b66e0fb960d1dfabcd3c7
kam-leung.so@siemens.com <kam-leung.so@siemens.com>
2021-11-04 05:07:12 +0000

    working one from Github

--- models/landing/segment/land_segment_identify.sql
+++ models/landing/segment/land_segment_identify.sql
@@ -8,7 +8,7 @@ SELECT {{ var("lnd_schema") }}.SEQ_LAND_SEGMENT_IDENTIFY.NEXTVAL LAND_SEGMENT_ID
       ,*
 FROM {{ source('aws_datalake','RAW_SEGMENT_IDENTIFY_T') }}
 
-{% if is_incremental() %}
+{% if is_incremental() %} 
 
   -- this filter will only be applied on an incremental run
   where {{ var("segment_identify_key") }} > (select max( {{ var("segment_identify_key") }}) from {{ this }})
